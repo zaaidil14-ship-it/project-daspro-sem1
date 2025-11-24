a0e2ace3c0aee6170a410ce4485bf8321c5d37ba
akbar <zaaidil14@gmail.com>
2025-11-24 14:38:04 +0700

    menambahkan fitur tampilan dan logika kemenangan

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
 //project akhir
+#include <stdio.h>
+
+
+// --- Fungsi Menampilkan Papan ---
+void displayBoard(char board[3][3]) {
+    printf("\n");
+    for (int i = 0; i < 3; i++) {
+        printf(" %c | %c | %c ", board[i][0], board[i][1], board[i][2]);
+        if (i < 2) printf("\n---|---|---\n");
+    }
+    printf("\n");
+}
+
+// --- Mengecek Kemenangan ---
+int checkWin(char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
+            return 1;
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
+            return 1;
+    }
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+        return 1;
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+        return 1;
+    return 0;
+}
